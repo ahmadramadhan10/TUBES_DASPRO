6539cbf5c23b9121fd4cf8aba6aa08ef628419e6
david-dewanto <111259931+david-dewanto@users.noreply.github.com>
2023-04-19 22:12:33 +0700

    Delete F02.py

--- F02.py
+++
@@ -1,5 +0,0 @@
-def logout(current_login, can_access): 
-    if current_login == -1:
-        print("Logout gagal!")
-        print("Anda belum login, silahkan login terlebih dahulu sebelum melakukan logout")
-    return -1
