e4a0d20c1f8a6254e2af82eefedb76998bb81f24
unknown <aaarama21@gmail.com>
2023-04-21 02:34:30 +0800

    F06 clear coba di cek

--- F06JinPembangun.py
+++ F06JinPembangun.py
@@ -1,6 +1,34 @@
 import random
 
-def bangun():
+def bangun(bahan, sisa_candi):
+    if sisa_candi == 0:
+        print("Candi berhasil dibangun.")
+        print("Sisa candi yang perlu dibangun: {sisa_candi}")
+        return (bahan, sisa_candi)
+
     pasir = int(random.uniform(1,5))
     batu = int(random.uniform(1,5))
     air = int(random.uniform(1,5))
+    if bahan[0] >= pasir and bahan[1] >= batu and bahan[2] >= air:
+        bahan[0] = bahan[0] - pasir
+        bahan[1] = bahan[1] - batu
+        bahan[2] = bahan[2] - air
+        sisa_candi = sisa_candi - 1
+        print(f'Candi berhasil dibangun.')
+        print(f'Sisa candi yang perlu dibangun: {sisa_candi}.')
+    else:
+        print("Bahan bangunan tidak mencukupi.")
+        print("Candi tidak bisa dibangun!")
+    return (bahan,sisa_candi)
+
+
+# test case
+bahan = [100, 100, 100]
+sisa_candi = 100
+"""
+t = 5
+for i in range(t):
+    print(bangun(bahan,100))
+"""
+bahan, sisa_candi = bangun(bahan, sisa_candi)
+print(bahan, sisa_candi)
