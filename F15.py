83ea11b8f5509b1ef38b0dd57d43d3f5facd7cda
david-dewanto <111259931+david-dewanto@users.noreply.github.com>
2023-04-20 13:38:05 +0700

    Delete F15.py

--- F15.py
+++
@@ -1,6 +0,0 @@
-def help():
-    print("=========== HELP ===========")
-    print("1. login")
-    print("   Untuk masuk menggunakan akun")
-    print("2. exit")
-    print("   Untuk keluar dari program dan kembali ke terminal")
