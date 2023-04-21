272375f8704c5f025e32d312c88041851776a37e
unknown <aaarama21@gmail.com>
2023-04-21 13:15:16 +0800

    update

--- F11HancurkanCandi.py
+++ F11HancurkanCandi.py
@@ -1,8 +1,7 @@
+default = ["",0,0,0]
+
 def CekCandi(candi, id):
-    for i in range(102):
-        if candi[i][0] == id:
-            return True 
-    return False
+    return not (default == candi[id])
 
 def hancurkan(candi):
     id = int(input("Masukkan ID candi: "))
@@ -10,16 +9,14 @@ def hancurkan(candi):
     if exist:
         decision = input("Apakah anda yakin ingin menghancurkan candi ID: 5 (Y/N)? ")
         if decision == "Y":
-            for i in range(102):
-                if id == candi[i][0]:
-                    candi[i] = [0,"",0,0,0]
+            candi[id] = default
             print("\nCandi telah berhasil dihancurkan\n")
     else:
         print("\nTidak ada candi dengan ID tersebut.")
 
 #"""
 #test case
-candi = [[0, "", 0, 0, 0]]*102
-candi[0] = [1,"infokan", 5, 5, 5] 
+candi = [["", 0, 0, 0]]*102
+candi[0] = ["infokan", 5, 5, 5] 
 hancurkan(candi)
 #"""
