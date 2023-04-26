e6329240342d4979a9d4a9781c0f039d73c6e5bf
david-dewanto <111259931+david-dewanto@users.noreply.github.com>
2023-04-26 11:30:11 +0700

    Add files via upload

--- F16Exit.py
+++ F16Exit.py
@@ -4,7 +4,7 @@ def exit(jumlahusers, users, jumlahcandi, candi, bahan):
     konfirmasi = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
     while konfirmasi != 'y' and konfirmasi != 'n':
         konfirmasi = input("Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ")
-    if konfirmasi == "y":
+    if konfirmasi == "y" or konfirmasi == "Y":
         save(jumlahusers, users, jumlahcandi, candi, bahan)
     print("Keluar dari program...")
     print("Berhasil keluar dari program!\n")
